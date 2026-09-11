c57a53ca646fecbcce21b8130fd1074336477949
Sfatima22 <shaguftaf28@gmail.com>
2026-09-11 18:13:52 +0000

    completed 3rd exercise of topic 1

---
+++ 03.js
@@ -0,0 +1,15 @@
+console.log(5 == "5");
+console.log(5 === "5");
+
+console.log(0 == false);
+console.log(0 === false);
+
+console.log("" == false);
+console.log("" === false);
+
+// output true
+// false
+// true
+// false
+// true
+// false
