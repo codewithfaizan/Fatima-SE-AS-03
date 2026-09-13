fd0050b5849945f6b49ad2b0597fdc61d82b45b9
Sfatima22 <shaguftaf28@gmail.com>
2026-09-13 15:45:56 +0000

    29th ex completed

---
+++ 29.js
@@ -0,0 +1,12 @@
+let browser = "FireFox";
+
+if (browser === "Edge") {
+    console.log("You've got the Edge!");
+} else if (browser === "Chrome" || browser === "Firefox" || browser === "Safari" || browser === "Opera") {
+    console.log("Okay we support these browsers too");
+} else {
+    console.log("We hope that this program looks ok!");
+}
+
+// used AI fir line 1
+// We hope that this program looks ok!
