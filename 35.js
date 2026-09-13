119b9f84fdf4f55e92dbbf55cf469b2f90bcdcbd
Sfatima22 <shaguftaf28@gmail.com>
2026-09-13 17:32:41 +0000

    ex-35 completed

---
+++ 35.js
@@ -0,0 +1,24 @@
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//  
+// console.log(checkAge(15)); // false
+
+// used AI for 9-10 line 
+
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    }
+
+    return false;
+}
+console.log(checkAge(15));
+console.log(checkAge(70));
+
+// else does not change the behavior
