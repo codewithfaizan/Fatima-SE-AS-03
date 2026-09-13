609fb6604ff2adae403b77ae2c5b77d3d4e2c9a1
Sfatima22 <shaguftaf28@gmail.com>
2026-09-13 16:39:25 +0000

    ex-32 completed

---
+++ 32.js
@@ -0,0 +1,15 @@
+function showWelcome() {
+    console.log("Welcome To JavaScript");
+}
+                  
+showWelcome(); 
+showWelcome();  
+showWelcome();  
+showWelcome();   
+showWelcome();  
+
+// Welcome To JavaScript
+// Welcome To JavaScript
+// Welcome To JavaScript
+// Welcome To JavaScript
+// Welcome To JavaScript
