b0ae7808a4b087dabded265afc5ae92c4058fb41
Sfatima22 <shaguftaf28@gmail.com>
2026-09-13 16:49:31 +0000

    ex-34 completed

---
+++ 34.js
@@ -0,0 +1,19 @@
+function add(a , b) {
+    return a + b;
+}
+console.log(add(10,20));
+
+// used AI for line 2
+// 30
+
+function add(a , b) {
+    return a - b;
+}
+console.log(add(10,20));
+// -10
+
+function add(a , b) {
+    return a * b;
+}
+console.log(add(10,20));
+// 200
