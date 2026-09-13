a8e6e5df550da45b74bf1d0c6727547b9771c191
Sfatima22 <shaguftaf28@gmail.com>
2026-09-13 15:18:25 +0000

    26th ex completed

---
+++ 26.js
@@ -0,0 +1,23 @@
+// for (let i = 1; i < 20; i++) {
+//     if (i % 2 !=0) {
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+// 2
+// 4
+// 6
+// 8
+// 10
+// 12
+// 14
+// 16
+// 18
+
+for (let i = 1; i < 50; i++) {
+    if (i % 5 ===0) {
+    continue;
+    }
+    console.log(i);
+ }  
