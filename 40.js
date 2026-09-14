ea240e50489ab85845df3eabcf4d1caadc0474ad
Sfatima22 <shaguftaf28@gmail.com>
2026-09-14 07:33:52 +0000

    ex-40 completed

---
+++ 40.js
@@ -0,0 +1,14 @@
+
+const doubleExpr = n => n * 2;
+
+
+const doubleBlock = n => {
+    return n * 2;
+};
+
+console.log(doubleExpr(5)); // 10
+console.log(doubleBlock(5)); // 10
+
+// used AI 
+// 10
+// 10
